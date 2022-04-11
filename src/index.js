f33f89056dd2395cf0d90fae6a41d022ddcfa164
Iryna S <irynaradkovska@gmail.com>
2022-04-11 12:03:33 +0300

    оновлює модалку для команди
    рендер карток, трохи функціоналу, зміни в стилях тощо

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './sass/main.scss';
 import API from './api/fetch';
+import { refs } from './js/refs';
+import { teamMarkup } from './js/team-markup';
+
+// модалка для команди
+
+refs.openModalTeamBtn.addEventListener('click', toggleModalTeam);
+refs.closeModalTeamBtn.addEventListener('click', toggleModalTeam);
+refs.modalTeam.addEventListener('click', removeModalTeam);
+
+function toggleModalTeam() {
+  refs.teamContainer.insertAdjacentHTML('beforeend', teamMarkup);
+  refs.modalTeam.classList.toggle('is-hidden');
+}
+
+function removeModalTeam(e) {
+  if (e.target === refs.modalTeam) {
+    refs.modalTeam.classList.add('is-hidden');
+  }
+}
---
+++ src/js/team-items.js
@@ -0,0 +1,101 @@
+export const teamItems = [
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/default.jpg',
+    firstName: 'Olha',
+    secondName: 'Rosiak',
+    position: 'Team Lead',
+    github: 'https://github.com/oliarosiak',
+    linkedin: 'https://www.linkedin.com/in/olia-rosiak/',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/default.jpg',
+    firstName: 'Stanislav',
+    secondName: 'Protashchuk',
+    position: 'Scrum Master',
+    github: 'https://github.com/StanislavProtashchuk',
+    linkedin: 'https://www.linkedin.com/in/stanislav-protashchuk-317325226/',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/Ivanytskyi.jpg',
+    firstName: 'Oleh',
+    secondName: 'Ivanytskyi',
+    position: 'Developer',
+    github: 'https://github.com/ivanytskyi2407',
+    linkedin: 'https://www.linkedin.com/in/oleh-ivanytskyi-6b54851a0/',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/Hembarska.jpg',
+    firstName: 'Oleksandra',
+    secondName: 'Hembarska',
+    position: 'Developer',
+    github: 'https://github.com/Oleksandra9',
+    linkedin: '',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/Hrytsyshyn.jpg',
+    firstName: 'Vasyl',
+    secondName: 'Hrytsyshyn',
+    position: 'Developer',
+    github: 'https://github.com/VasylHrytsyshyn',
+    linkedin: '',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/default.jpg',
+    firstName: 'Marta',
+    secondName: 'Tsiruh',
+    position: 'Developer',
+    github: 'https://github.com/martatsiruh',
+    linkedin: '',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/default.jpg',
+    firstName: 'Oleksandr',
+    secondName: 'Lokshuk',
+    position: 'Developer',
+    github: 'https://github.com/12Sasha21',
+    linkedin: '',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/default.jpg',
+    firstName: 'Oksana',
+    secondName: 'Khmil',
+    position: 'Developer',
+    github: 'https://github.com/Oksana-Khmil',
+    linkedin: '',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/default.jpg',
+    firstName: 'Mykola',
+    secondName: 'Havryliuk',
+    position: 'Developer',
+    github: 'https://github.com/nik-349',
+    linkedin: '',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/default.jpg',
+    firstName: 'Oleksandr',
+    secondName: 'Boliachevets',
+    position: 'Developer',
+    github: 'https://github.com/Schamotnic',
+    linkedin: '',
+  },
+  {
+    image:
+      'https://raw.githubusercontent.com/oliarosiak/filmoteka-project/main/src/images/team/Radkovska.jpg',
+    firstName: 'Iryna',
+    secondName: 'Radkovska',
+    position: 'Developer',
+    github: 'https://github.com/Akrisia',
+    linkedin: 'https://www.linkedin.com/in/iryna-radkovska-343698159/',
+  },
+];
