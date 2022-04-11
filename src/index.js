08733e9431544d1e28f8427fb9e8e09cfab81f90
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-04-11 14:56:11 +0300

    header/signIn-out

--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ import './sass/main.scss';
 import API from './api/fetch';
 import { refs } from './js/refs';
 import { teamMarkup } from './js/team-markup';
+import './js/authFirebase';
 
 // модалка для команди
 
-refs.openModalTeamBtn.addEventListener('click', toggleModalTeam);
-refs.closeModalTeamBtn.addEventListener('click', toggleModalTeam);
-refs.modalTeam.addEventListener('click', removeModalTeam);
+// наразі закоментував бо багато помилок в консолі
+// refs.openModalTeamBtn.addEventListener('click', toggleModalTeam);
+// refs.closeModalTeamBtn.addEventListener('click', toggleModalTeam);
+// refs.modalTeam.addEventListener('click', removeModalTeam);
 
-function toggleModalTeam() {
-  refs.teamContainer.insertAdjacentHTML('beforeend', teamMarkup);
-  refs.modalTeam.classList.toggle('is-hidden');
-}
+// function toggleModalTeam() {
+//   refs.teamContainer.insertAdjacentHTML('beforeend', teamMarkup);
+//   refs.modalTeam.classList.toggle('is-hidden');
+// }
 
-function removeModalTeam(e) {
-  if (e.target === refs.modalTeam) {
-    refs.modalTeam.classList.add('is-hidden');
-    refs.teamContainer.innerHTML = '';
-  }
-}
+// function removeModalTeam(e) {
+//   if (e.target === refs.modalTeam) {
+//     refs.modalTeam.classList.add('is-hidden');
+//     refs.teamContainer.innerHTML = '';
+//   }
+// }
