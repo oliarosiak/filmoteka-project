4d5af81179fdc354298eea6ca3439e6f0a0559c3
Oleksandra9 <sashakrytska@gmail.com>
2022-04-14 11:34:35 +0100

    +

--- src/js/cardModal.js
+++ src/js/cardModal.js
@@ -1,34 +1,36 @@
-import { refs } from './refs';
+
 
 //---------------------
-import { getMovies } from './fetch'
-// import { getMovies } from './js/fetch';
-// import { renderCardMurkup } from './js/renderCard';
+// import { getMovies } from './fetch'
+import { getFilmById } from './fetch';
 
-
-getMovies().then((data) => renderCardMurkup(data)).catch((e) => {
-    console.log(e)
-});
-
-
+// getMovies().then((data) => renderCardMurkup(data)).catch((e) => {
+//     console.log(e)
+// });
 
 // ------------
 const openModal = document.querySelector('.film-list');
 const backdropCardModal = document.querySelector(".backdrop--card");
-// const boxCardModal = document.querySelector(".card-modal");
+const boxCardModal = document.querySelector(".card-modal__discription-box");
 console.log(openModal);
 console.log(backdropCardModal);
-// console.log(boxCardModal);
+console.log(boxCardModal);
 
 openModal.addEventListener("click", openModalCard);
 
-function openModalCard (event) {
-    if (!event.target.closest('.card__link')) {
-        return;
-      }
-   
-    console.log(event.target);
+function openModalCard(e) {
+    const curLink = e.target.closest('.card__link');
+    if (!curLink) {
+      return;
+    }
+    const curFilm = curLink.id;
+    getFilmById(curFilm)
+      .then(data => {
+        //  ЗРОБИТИ РЕНДЕР
+        console.log(data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
     backdropCardModal.classList.remove('is-hidden');
-
-    
-}
+  }
