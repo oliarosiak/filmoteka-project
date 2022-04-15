fb251e0aa8a6a634c790c4ebd468455233db7686
Vasyl Hrytsyshyn <matsjo19@gmail.com>
2022-04-15 21:12:58 +0300

    aaaa

--- src/js/render-сard.js
+++ src/js/render-сard.js
@@ -1,7 +1,9 @@
 import { refs } from './refs';
 import { genres } from './genres.json';
 
-//функція рендеру карток фільму на сторінці HOME
+const BASE_IMG_URL = 'http://image.tmdb.org/t/p/w300';
+
+//функція рендеру карток фільму в HOME
 export function renderCardMurkup(data) {
   const galleryMarkup = data.results
     .map(({ id, poster_path, title, genre_ids, release_date }) => {
@@ -13,11 +15,11 @@ export function renderCardMurkup(data) {
         .join(', ');
       return `<li class="card">
                     <a href="#${id}" class="card__link" id="${id}">
-                        <img class="card__img" src="http://image.tmdb.org/t/p/w300${poster_path}" alt="photo" />
+                        <img class="card__img" src="${BASE_IMG_URL}${poster_path}" alt="photo" />
                         <div class="card__information">
                             <h2 class="card__name">${title}</h2>
                             <div class="card__inf">
-                            <p class="card__genre">${genreName} | ${release_date.slice(0,4,)}</p>    
+                            <p class="card__genre">${genreName} | ${release_date.slice(0,4)}</p>    
                             </div>
                         </div>
                     </a>
@@ -27,18 +29,18 @@ export function renderCardMurkup(data) {
   refs.homeFilmList.insertAdjacentHTML('beforeend', galleryMarkup);
 }
 
-//функція рендеру карток фільму на сторінці MY LIBRARY
+//функція рендеру карток фільму в MY LIBRARY
 
 export function renderCardMurkupLibreary(data) {
   const galleryMarkupLibreary = data
     .map(({ id, poster_path, title, genres, release_date, vote_average }) => {
       return `<li class="card">
                     <a href="#${id}" class="card__link" id="${id}">
-                        <img class="card__img" src="http://image.tmdb.org/t/p/w300${poster_path}" alt="photo" />
+                        <img class="card__img" src="${BASE_IMG_URL}${poster_path}" alt="photo" />
                         <div class="card__information">
                             <h2 class="card__name">${title}</h2>
                             <div class="card__inf">   
-                            <p class="card__genre">${genres.name} | ${release_date.slice(0,4,)}</p>                         
+                            <p class="card__genre">${genres.name} | ${release_date.slice(0,4)}</p>                         
                             <p class="card__rating" > ${vote_average}</p>
                             </div>
                         </div>
@@ -46,5 +48,5 @@ export function renderCardMurkupLibreary(data) {
                 </li>`;
     })
     .join('');
-  refs.libraryFilmList.insertAdjacentHTML('beforeend', galleryMarkupLibreary);
+  refs.homeFilmList.insertAdjacentHTML('beforeend', galleryMarkupLibreary); // змінено на refs.homeFilmList
 }
