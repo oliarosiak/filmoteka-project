c9ca58afe11f701e1239c26fd1e5e4303ff01258
nik-349 <nik349@icloud.com>
2022-04-15 15:58:12 +0300

    bar

--- src/js/libraryButton.js
+++ src/js/libraryButton.js
@@ -46,13 +46,11 @@ function clickButWatched (){
 };
 
 
+window.onscroll = function() {startBar()};
 
-
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+function startBar() {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
