739ea08e2fba6e58111dbd6b5f7b9a971e6486f3
Iryna S <irynaradkovska@gmail.com>
2022-04-16 17:52:12 +0300

    додає дрібні правки

--- src/js/header-fixed.js
+++ src/js/header-fixed.js
@@ -2,7 +2,7 @@ const headerFixed = document.querySelector('.header');
 document.addEventListener('scroll', onHeaderFixed);
 
 function onHeaderFixed() {
-  if (window.scrollY > 220) {
+  if (window.scrollY > 60) {
     headerFixed.classList.add('header--fixed');
   } else {
     headerFixed.classList.remove('header--fixed');
