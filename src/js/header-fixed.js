c6f6dff4c2eabc3522b928d063a3b236c13c11ed
Iryna S <irynaradkovska@gmail.com>
2022-04-16 13:19:51 +0300

    фіксує футер, хедер, розташовує модалку команди

---
+++ src/js/header-fixed.js
@@ -0,0 +1,1 @@
+import { scrollPage } from './scroll-btn';
--- src/js/scroll-btn.js
+++ src/js/scroll-btn.js
@@ -4,15 +4,15 @@ document.addEventListener('scroll', scrollPage);
 // window.addEventListener('scroll', scrollPage);
 scrollBtn.addEventListener('click', scrollToTop);
 
-function scrollPage() {
-    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-        scrollBtn.style.display = "block";
-    } else {
-        scrollBtn.style.display = "none";
-    }  
+export function scrollPage() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    scrollBtn.style.display = 'block';
+  } else {
+    scrollBtn.style.display = 'none';
+  }
 }
 
 function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;  
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
