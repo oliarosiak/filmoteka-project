6388e2340c082eba51ecf4d968d3e4203dd87631
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-04-16 15:20:24 +0300

    +

--- src/js/cardModalMurkup.js
+++ src/js/cardModalMurkup.js
@@ -1,6 +1,6 @@
 // ` <div class="card-modal__image-box">
 // <img class="card-modal__banner" src="images/team/default.jpg" alt="">
-// </div>       
+// </div>
 // <div class="card-modal__information-box">
 // <h2 class="card-modal__title">render</h2>
 // <div class="card-modal__wrapper">
@@ -20,19 +20,18 @@
 //     <p class="card-modal__wrapper-value card-modal__wrapper-value--size">render</p>
 // </div>
 // <h3 class="card-modal__about-title">About</h3>
-// <p class="card-modal__about-text">render</p> 
+// <p class="card-modal__about-text">render</p>
 // <div class="card-modal__button-box">
 //     <button type="button" class="card-modal__button-add-watched ">add to watched</button>
 //     <button type="button" class="card-modal__button-add-queue">add to queue</button>
-// </div>    
+// </div>
 // </div>`
 
-const boxCardModal = document.querySelector(".card-modal__container");
+const boxCardModal = document.querySelector('.card-modal__container');
 
-export function renderCardModalMurkup (data) {
-    const cardModalmurkup = data.map(({ title}) => {
-        return `<div class="card-modal__image-box">
-        <img class="card-modal__banner" src="" alt="">
+export function renderCardModalMurkup({ title, backdrop_path }) {
+  const cardModalmurkup = `<div class="card-modal__image-box">
+        <img class="card-modal__banner" src="http://image.tmdb.org/t/p/w300${backdrop_path}" alt="">
         </div>       
         <div class="card-modal__information-box">
         <h2 class="card-modal__title">${title}</h2>
@@ -54,8 +53,7 @@ export function renderCardModalMurkup (data) {
         </div>
         <h3 class="card-modal__about-title">About</h3>
         <p class="card-modal__about-text">render</p>     
-        </div>`
-    }).join('');
-    boxCardModal.insertAdjacentHTML('beforeend', cardModalmurkup);
-    console.log(data.original_title)
+        </div>`;
+  boxCardModal.insertAdjacentHTML('beforeend', cardModalmurkup);
+  console.log(data.original_title);
 }
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -5,10 +5,12 @@ const BASE_URL = 'https://api.themoviedb.org/3/';
 const KEY = '084ca305e7a4e4bb3dbbc7b67e975385';
 
 export async function getMovies(query, page) {
-  const response = await axios.get(`${BASE_URL}${search}?api_key=${KEY}&language=en-US&query=${query}&page=${page}`);
+  const response = await axios.get(
+    `${BASE_URL}${search}?api_key=${KEY}&language=en-US&query=${query}&page=${page}`,
+  );
   try {
     const data = response.data;
-    console.log(data);
+    // console.log(data);
     return data;
   } catch (error) {
     console.log(error);
@@ -17,7 +19,7 @@ export async function getMovies(query, page) {
 
 export async function getFilmById(filmId) {
   try {
-    const axiosFilmId = await axios.get(`${BASE_URL}${filmId}?api_key=${KEY}`);
+    const axiosFilmId = await axios.get(`${BASE_URL}movie/${filmId}?api_key=${KEY}`);
     return axiosFilmId.data;
   } catch (error) {
     console.log(error);
