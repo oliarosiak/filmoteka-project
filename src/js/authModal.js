d1ed541c47c1f17d693df7d4f53a820a8c9349f9
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-04-19 13:37:27 +0300

    registerModel - done

--- src/js/authModal.js
+++ src/js/authModal.js
@@ -1,14 +1,36 @@
 import { refs } from './refs';
 import { onKeyPress, onBackdropClick } from './closeModal.js';
+
+function showHideSignInModel() {
+  refs.signInForm.classList.toggle('is-hidden');
+}
+function showHideRegisterModal() {
+  refs.registerForm.classList.toggle('is-hidden');
+}
+function clickBackdrop(e) {
+  if (e.target === e.currentTarget) {
+    showHideRegisterModal();
+  }
+  refs.registerForm.removeEventListener('click', clickBackdrop);
+}
+function clickEsc(e) {
+  if (e.code === 'Escape') {
+    showHideRegisterModal();
+  }
+  refs.registerForm.removeEventListener('keydown', clickEsc);
+}
+function clickBtn() {
+  showHideRegisterModal();
+  refs.registerModal__closebtn.removeEventListener('click', clickBtn);
+}
+
 refs.openRegisterForm.addEventListener('click', () => {
-  refs.registerForm.classList.toggle('is-hidden');
+  showHideRegisterModal();
+  refs.registerForm.addEventListener('click', clickBackdrop);
+  refs.registerModal__closebtn.addEventListener('click', clickBtn);
+  refs.registerForm.addEventListener('keydown', clickEsc);
 });
-refs.registerModal__closebtn.addEventListener('click', () => {
-  refs.registerForm.classList.toggle('is-hidden');
-});
+
 refs.openSignInForm.addEventListener('click', () => {
-  refs.signInForm.classList.toggle('is-hidden');
-});
-refs.signInModal__closebtn.addEventListener('click', () => {
-  refs.signInForm.classList.toggle('is-hidden');
+  showHideSignInModel();
 });
