87650b7c49f0411a967370cd1ab3d1606025c3b3
Oleksandra9 <sashakrytska@gmail.com>
2022-04-19 03:22:58 +0100

    +

--- src/js/cardModal.js
+++ src/js/cardModal.js
@@ -1,15 +1,19 @@
 import { getFilmById } from './fetch';
-import { getMovies } from './fetch';
 import { renderCardModalMurkup } from './cardModalMurkup';
 
 const openModal = document.querySelector('.film-list');
 const backdropCardModal = document.querySelector('.backdrop--card');
 const boxCardModal = document.querySelector('.card-modal__container');
+
+
+const buttonCloseModal = document.querySelector(".card-modal__button-close");
+
 // console.log(openModal);
 // console.log(backdropCardModal);
 // console.log(boxCardModal);
 
 openModal.addEventListener('click', openModalCard);
+buttonCloseModal.addEventListener("click",onCloseButtonClick);
 
 function openModalCard(e) {
   const curLink = e.target.closest('.card__link');
@@ -21,7 +25,7 @@ function openModalCard(e) {
   getFilmById(curFilm)
     .then(data => {
       // console.log(data);
-      renderCardModalMurkup(data);
+      boxCardModal.insertAdjacentHTML('beforeend', renderCardModalMurkup(data));
     })
     .catch(e => {
       console.log(e);
@@ -29,3 +33,21 @@ function openModalCard(e) {
 
   backdropCardModal.classList.remove('is-hidden');
 }
+
+//-------- Кнопка закриття
+// МАРТА, якщо ти не заливала код на кнопку закриття модалки- то цей код вроді робочий) 
+
+// function onCloseButtonClick() {
+//   const backdropCardModal = document.querySelector('.backdrop--card');
+//   const cardModalInfo = document.querySelector('.card-modal__information-box');
+//   const cardModalImg = document.querySelector('.card-modal__image-box');
+
+//   cardModalImg.remove();
+//   cardModalInfo.remove();
+
+//   backdropCardModal.classList.add('is-hidden');
+
+//   document.body.style.overflow = 'scroll';
+  
+// }
+
