e2592d8a18836acf4b04f3bbcc1815fcc10ca0fc
Oleksandra9 <sashakrytska@gmail.com>
2022-04-20 11:33:19 +0100

    Update buttonsclose

--- src/js/authModal.js
+++ src/js/authModal.js
@@ -1,22 +1,48 @@
 import { refs } from './refs';
 import { onKeyPress, onBackdropClick } from './closeModal.js';
+
+//----------- SIGN IN
+
+// відкриття модалки
+refs.openSignInForm.addEventListener('click', showHideSignInModel);
+
 function showHideSignInModel() {
   refs.signInForm.classList.toggle('is-hidden');
-  refs.signInModal__closebtn.addEventListener('click', () => {
-    refs.signInForm.classList.toggle('is-hidden');
-    refs.openModalTeamBtn.removeEventListener('click', showHideSignInModel);
-  });
+  
+  };
+
+// закриття модалки по кліку на кнопку закриття, по кліку на бекдроп
+refs.signInModal__closebtn.addEventListener('click', onCloseSignInAuthModal);
+refs.signInForm.addEventListener("click", onCloseSignInAuthModal);
+
+function onCloseSignInAuthModal (e) {
+  if (e.target === refs.signInForm ||
+    e.currentTarget === refs.signInModal__closebtn ||
+    e.code === 'Escape') {
+      refs.signInForm.classList.toggle('is-hidden');
+  }
 }
-function showHideRegisterModal() {
-  refs.registerForm.classList.toggle('is-hidden');
-  refs.registerModal__closebtn.addEventListener('click', () => {
-    refs.registerForm.classList.toggle('is-hidden');
-    refs.openRegisterForm.removeEventListener('click', showHideRegisterModal);
-  });
-}
-refs.openSignInForm.addEventListener('click', showHideSignInModel);
+
+//----------- SIGN UP
+// відкриття модалки
 refs.openRegisterForm.addEventListener('click', showHideRegisterModal);
 
+function showHideRegisterModal() {
+  refs.registerForm.classList.toggle('is-hidden');
+  };
+// закриття модалки по кліку на кнопку закриття, по кліку на бекдроп
+refs.registerModal__closebtn.addEventListener('click', onCloseSignUpAuthModal);
+refs.registerForm.addEventListener("click",onCloseSignUpAuthModal);
+
+function onCloseSignUpAuthModal(e) {
+  if (e.target === refs.registerForm ||
+    e.currentTarget === refs.registerModal__closebtn ||
+    e.code === 'Escape') {
+      refs.registerForm.classList.toggle('is-hidden');
+  }
+}
+
+// ------------Олега код
 // function clickBackdrop(e) {
 //   if (e.target === e.currentTarget) {
 //     showHideRegisterModal();
